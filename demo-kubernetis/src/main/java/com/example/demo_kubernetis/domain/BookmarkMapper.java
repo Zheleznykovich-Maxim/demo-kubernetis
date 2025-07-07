5f342fbd529224d889d6bb8d5dd74ea6e57a64b1
Железнякович Максим <m.zheleznyakovich@bft.ru>
2025-07-07 15:40:09 +0300

    lesson 5 - jpa projections

---
+++ demo-kubernetis/src/main/java/com/example/demo_kubernetis/domain/BookmarkMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo_kubernetis.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookmarkMapper {
+
+    public BookmarkDTO toDto(Bookmark bookmark) {
+        BookmarkDTO bookmarkDTO = new BookmarkDTO();
+        bookmarkDTO.setId(bookmark.getId());
+        bookmarkDTO.setTitle(bookmark.getTitle());
+        bookmarkDTO.setUrl(bookmark.getUrl());
+        bookmarkDTO.setCreatedAt(bookmark.getCreatedAt());
+        return bookmarkDTO;
+    }
+}
--- demo-kubernetis/src/main/java/com/example/demo_kubernetis/domain/BookmarkService.java
+++ demo-kubernetis/src/main/java/com/example/demo_kubernetis/domain/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.example.demo_kubernetis.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -12,11 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
+    private final BookmarkMapper bookmarkMapper;
 
     @Transactional(readOnly = true)
     public BookmarksDTO getBookmarks(Integer page) {
         int pageNumber = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNumber, 10, Sort.Direction.DESC, "createdAt");
-        return new BookmarksDTO(bookmarkRepository.findAll(pageable));
+        Page<BookmarkDTO> bookmarkPage = bookmarkRepository.findBookmarks(pageable);
+        return new BookmarksDTO(bookmarkPage);
     }
 }
